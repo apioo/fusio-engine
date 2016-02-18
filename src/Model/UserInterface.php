22b67f6110ba51c3bf643730406beecde5e62e42
Christoph Kappestein <k42b3.x@gmail.com>
2016-02-18 18:58:36 +0100

    add user model and loader

---
+++ src/Model/UserInterface.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Engine\Model;
+
+/**
+ * UserInterface
+ *
+ * @author  Christoph Kappestein <k42b3.x@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+interface UserInterface
+{
+    const STATUS_CONSUMER      = 0;
+    const STATUS_ADMINISTRATOR = 1;
+    const STATUS_DISABLED      = 2;
+    const STATUS_DELETED       = 3;
+
+    /**
+     * @return integer
+     */
+    public function getId();
+    
+    /**
+     * @return integer
+     */
+    public function getStatus();
+    
+    /**
+     * @return string
+     */
+    public function getName();
+}
