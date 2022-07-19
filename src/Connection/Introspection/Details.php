6ca145ef33c915e13f61f8d79a5edb0b30f571af
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-07-19 20:55:50 +0200

    add introspection interface

---
+++ src/Connection/Introspection/Details.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015-2022 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Engine\Connection\Introspection;
+
+/**
+ * Details
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    https://www.fusio-project.org
+ */
+class Details implements \JsonSerializable
+{
+    private string $name;
+    private array $headers;
+    private array $rows;
+
+    public function __construct(string $name, array $headers)
+    {
+        $this->name = $name;
+        $this->headers = $headers;
+        $this->rows = [];
+    }
+
+    public function addRow(array $row): void
+    {
+        if (count($row) !== count($this->headers)) {
+            throw new \InvalidArgumentException('Row must match the headers count');
+        }
+
+        $this->rows[] = array_values($row);
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'name' => $this->name,
+            'headers' => $this->headers,
+            'rows' => $this->rows,
+        ];
+    }
+}
