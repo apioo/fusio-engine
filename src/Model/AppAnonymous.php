e2e9f9bbd9620c3e04b099dd7c6dda396031ef39
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-03-24 21:28:15 +0100

    add app, token and user anonymous instance

---
+++ src/Model/AppAnonymous.php
@@ -0,0 +1,93 @@
+<?php
+/*
+ * Fusio is an open source API management platform which helps to create innovative API solutions.
+ * For the current version and information visit <https://www.fusio-project.org/>
+ *
+ * Copyright 2015-2023 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Fusio\Engine\Model;
+
+/**
+ * AppAnonymous
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    https://www.fusio-project.org
+ */
+class AppAnonymous implements AppInterface
+{
+    public function isAnonymous(): bool
+    {
+        return true;
+    }
+
+    public function getId(): int
+    {
+        return 0;
+    }
+
+    public function getUserId(): int
+    {
+        return 0;
+    }
+
+    public function getStatus(): int
+    {
+        return 0;
+    }
+
+    public function getName(): string
+    {
+        return '';
+    }
+
+    public function getUrl(): string
+    {
+        return '';
+    }
+
+    public function getAppKey(): string
+    {
+        return '';
+    }
+
+    public function getScopes(): array
+    {
+        return [];
+    }
+
+    public function hasScope(string $name): bool
+    {
+        return false;
+    }
+
+    public function getParameters(): array
+    {
+        return [];
+    }
+
+    public function getParameter(string $name): mixed
+    {
+        return null;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'anonymous' => true,
+        ];
+    }
+}
---
+++ src/Model/UserAnonymous.php
@@ -0,0 +1,88 @@
+<?php
+/*
+ * Fusio is an open source API management platform which helps to create innovative API solutions.
+ * For the current version and information visit <https://www.fusio-project.org/>
+ *
+ * Copyright 2015-2023 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Fusio\Engine\Model;
+
+/**
+ * UserAnonymous
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    https://www.fusio-project.org
+ */
+class UserAnonymous implements UserInterface
+{
+    public function isAnonymous(): bool
+    {
+        return true;
+    }
+
+    public function getId(): int
+    {
+        return 0;
+    }
+
+    public function getRoleId(): int
+    {
+        return 0;
+    }
+
+    public function getCategoryId(): int
+    {
+        return 0;
+    }
+
+    public function getStatus(): int
+    {
+        return 0;
+    }
+
+    public function getName(): string
+    {
+        return '';
+    }
+
+    public function getEmail(): string
+    {
+        return '';
+    }
+
+    public function getPoints(): int
+    {
+        return 0;
+    }
+
+    public function getExternalId(): ?string
+    {
+        return null;
+    }
+
+    public function getPlanId(): ?string
+    {
+        return null;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'anonymous' => true,
+        ];
+    }
+}
